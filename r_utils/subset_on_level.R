88b4cbc7729e263ca7984494175e5e1b011f5ea3
Lunkyadi <lunky.sucipto@gmail.com>
2023-05-10 02:40:29 +0200

    fixed bugs in data_exploration code

--- r_utils/subset_on_level.R
+++ r_utils/subset_on_level.R
@@ -4,7 +4,7 @@ subset_on_level = function(data, level_obj) {
   for (fact_col in names(Filter(is.factor, data))) {
     if (fact_col != "CODPOSS") {
       # below is monstrous but it's the only way I know
-      tmp = tmp[tmp[fact_col] == as.character(level_1[fact_col][1, 1]), ]
+      tmp = tmp[tmp[fact_col] == as.character(level_obj[fact_col][1, 1]), ]
     }
   }
   return (tmp)
